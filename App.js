a8b975b4108e0aa6292b8f430140cfd3fa1bbce0
arri <ewws@mail.ru>
2019-08-19 18:43:37 +0900

    new features

--- App.js
+++ App.js
@@ -8,17 +8,32 @@ import {
     Button,
     StatusBar,
 } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import {gql} from 'apollo-boost';
 
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+});
 const App = () => {
     return (
         <Fragment>
             <StatusBar barStyle="dark-content"/>
             <SafeAreaView>
-                <Button title={"test"}/>
+                <Button title={'test'} onPress={testClick}/>
 
             </SafeAreaView>
         </Fragment>
     );
 };
+const getHello = gql`
+    {
+        hello
+    }
+`;
+const testClick = async () => {
+   // console.log('yeah');
+    let {data,loading,errors} = await client.query({query: getHello});
+    console.log(data,loading,errors);
 
+};
 export default App;
